ae19ee94d16a83e77ad62562e87a2e243cbeece6
David <DavidOmarFCh@gmail.com>
2017-08-09 16:10:36 -0500

    Generates consensus matrix

--- rosalind.info/problems_list/Consensus Profile/ConsensusProfile.c
+++ rosalind.info/problems_list/Consensus Profile/ConsensusProfile.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define MOST_STRINGS 10
 
 typedef struct
 {
     char ID[14];
-    char string[1001] ;
+    char string[1001];
 } DNAString;
 
-void getDNAString(FILE *fp, char* s);
+void getDNAString(FILE *fp, char *s);
+void countNucleotides(DNAString *strings, int numStrings, int ith, int *nucleotides);
 
 int main(void)
-{    
+{
     FILE *fpInput = fopen("rosalind_cons.txt", "r");
 
     DNAString strings[MOST_STRINGS];
@@ -24,16 +27,58 @@ int main(void)
         if (strings[numStrings].ID[0] == '\0' || strings[numStrings].string[0] == '\0')
             break;
     }
+    int dnaLength = strlen(strings[0].string);
+    int **matrix = malloc(sizeof(int *) * dnaLength);
 
-    for (int i = 0; i < numStrings; i += 1)
+    for (int i = 0; i < dnaLength; i++)
     {
-        puts(strings[i].ID);
-        puts(strings[i].string);
+        matrix[i] = calloc(sizeof(int), 4);
     }
+
+    for (int i = 0; i < dnaLength; i++)
+    {
+        countNucleotides(strings, numStrings, i, matrix[i]);
+    }
+    
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < dnaLength; j++){
+            printf("%d ", matrix[j][i]);
+        }
+        printf("\n");
+    }
+
     return 0;
 }
 
-void getDNAString(FILE *fp, char* s)
+void countNucleotides(DNAString *strings, int numStrings, int ith, int *nucleotides)
+{
+    for (int i = 0; i < numStrings; i++)
+    {
+        if (strings[i].string[ith] == 'A')
+        {
+            nucleotides[0]++;
+            continue;
+        }
+        if (strings[i].string[ith] == 'C')
+        {
+            nucleotides[1]++;
+            continue;
+        }
+        if (strings[i].string[ith] == 'G')
+        {
+            nucleotides[2]++;
+            continue;
+        }
+        if (strings[i].string[ith] == 'T')
+        {
+            nucleotides[3]++;
+            continue;
+        }
+    }
+    return;
+}
+
+void getDNAString(FILE *fp, char *s)
 {
     int i = 0;
     do
