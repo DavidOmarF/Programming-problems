f3530c63cb67c59953600062fd015f107cbf3219
DavidOmarF <DavidOmarFCh@gmail.com>
2017-08-24 00:36:37 -0500

    019_prtm Rosalind, works fine in C

--- rosalind.info/problems_list/019_prtm_proteinMass/proteinMass.c
+++ rosalind.info/problems_list/019_prtm_proteinMass/proteinMass.c
@@ -1,15 +1,18 @@
+// For problem background, go to:
+// http://rosalind.info/problems/prtm/
+
 #include <stdio.h>
 #include <stdlib.h>
 
 void scanString(FILE *fp, char *string, int *stringSize);
-int CombinationsRNA(char *string, int size);
-int getValue(char a);
+double CalculateMass(char *string, int size);
+double getValue(char a);
 
 int main(void)
 {
     // open files
-    FILE *fpInput = fopen("rosalind_mrna.txt", "r");
-    FILE *fpOutput = fopen("rosalind_mrna_out.txt", "w");
+    FILE *fpInput = fopen("rosalind_prtm.txt", "r");
+    FILE *fpOutput = fopen("rosalind_prtm_out.txt", "w");
 
     // create and scan string
     char *string = malloc(1001 * sizeof(char));
@@ -17,36 +20,43 @@ int main(void)
     scanString(fpInput, string, &stringSize);
 
     // calcualte possible combinations
-    int combinations = CombinationsRNA(string, stringSize);
-    fprintf(fpOutput, "%d", combinations);
+    double mass = CalculateMass(string, stringSize);
+    fprintf(fpOutput, "%.9lf", mass);
 
     return 0;
 }
 
-int CombinationsRNA(char *string, int size)
+double CalculateMass(char *string, int size)
 {
-    int combinations = 1;
+    double mass = 0.0;
     for (int i = 0; i < size; i++)
-    {
-        combinations *= getValue(string[i]);
-        combinations = combinations % 1000000;
-    }
-    return combinations;
+        mass += getValue(string[i]);
+    return mass;
 }
 
-int getValue(char a)
+double getValue(char a)
 {
-    if (a == 'W' || a == 'M')
-        return 1;
-    if (a == 'Y' || a == 'C' || a == 'H' || a == 'Q' || a == 'N' ||
-        a == 'K' || a == 'D' || a == 'E' || a == 'F')
-        return 2;
-    if (a == 'I' || a == '\0')
-        return 3;
-    if (a == 'P' || a == 'T' || a == 'V' || a == 'A' || a == 'G')
-        return 4;
-    if (a == 'L' || a == 'S' || a == 'R')
-        return 6;
+    if (a == 'A') return 71.03711;
+    if (a == 'C') return 103.00919;
+    if (a == 'D') return 115.02694;
+    if (a == 'E') return 129.04259;
+    if (a == 'F') return 147.06841;
+    if (a == 'G') return 57.02146;
+    if (a == 'H') return 137.05891;
+    if (a == 'I') return 113.08406;
+    if (a == 'K') return 128.09496;
+    if (a == 'L') return 113.08406;
+    if (a == 'M') return 131.04049;
+    if (a == 'N') return 114.04293;
+    if (a == 'P') return 97.05276;
+    if (a == 'Q') return 128.05858;
+    if (a == 'R') return 156.10111;
+    if (a == 'S') return 87.03203;
+    if (a == 'T') return 101.04768;
+    if (a == 'V') return 99.06841;
+    if (a == 'W') return 186.07931;
+    if (a == 'Y') return 163.06333;
+    return 0.0;
 }
 
 void scanString(FILE *fp, char *string, int *stringSize)
@@ -58,6 +68,6 @@ void scanString(FILE *fp, char *string, int *stringSize)
         i++;
     }
     string[i - 1] = '\0';
-    *stringSize = i;
+    *stringSize = i - 1;
     return;
 }
